36bad404db7b40dfe49fe08c70de3f7ebd84b4a9
shivani chahar <shivanichahar443@gmail.com>
2025-11-01 21:06:14 +0530

    Update KBC  like Quiz.py

--- KBC  like Quiz.py
+++ KBC  like Quiz.py
@@ -5,20 +5,20 @@ questions=[
 ["Where is India's biggest slum area?","Delhi","Kolkata","Uttar Pradesh","Mumbai",4],
 ["What is the colore of sky?","Pink","Green","Yellow","Blue",4],
 ["Who is the current Prime Minister of India?", "Narendra Singh","Jawaharlal Nehru","Indira Gandhi","Narendra Modi",4]
-]
+]     # multiple lists in a single list(questions) also added the correct answer's option in the end of each list 
 
-levels=[1000,3000,5000,7000,10000]
-i=0
-for i in range(len(questions)):
+levels=[1000,3000,5000,7000,10000] #prize money for each question
+i=0#not really needed
+for i in range(len(questions)):#loops in the list named questions
     question=questions[i]  #gets the current question and assigns it to the variable question. 
     print(f"Question for Rs {levels[i]}") #question for Rs 1000,3000,5000,etc
     print(question[0])
-    print(f"a.{question[1]}    b.{question[2]})")
-    print(f"c.{question[3]}    d.{question[4]})")
-    reply=int(input("Enter Your Option\n"))
-    if (reply==question[5]):
-       print("Correct Answer!!!")
+    print(f"a.{question[1]}    b.{question[2]})")#{question[1]}gives option 1 {question[2]}gives option 2 and same for 3 and 4 
+    print(f"c.{question[3]}    d.{question[4]})")#basically 1,2,3,4 are the indexs of the options 
+    reply=int(input("Enter Your Option\n"))#will take the user input  for the answer
+    if (reply==question[5]): #question[5] basically he 5th index element stores the correct option number (1,2,3 or 4).
+       print("Correct Answer!!!") #if both match then correct answer 
     else:
-       print("Wrong Answer!!!")
-       break
+       print("Wrong Answer!!!")# did't match wrong answer 
+       break#if wrong answer stop the game
 print("\n*******Quiz is Finished*******")
